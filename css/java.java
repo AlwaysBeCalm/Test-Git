39938b37e60b749946ab54c9eaee193e26c7f78c
JustAnotherOne <abdullah@pop-os.localdomain>
2020-01-16 17:15:03 +0300

    added var and print

--- css/java.java
+++ css/java.java
@@ -2,5 +2,7 @@ class java {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Java2");
+        int x = 10;
+        System.out.println(x);
     }
 }
